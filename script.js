9ec6dcebd006c3ef387bc9ee01edb363bd7a843f
myratdev <myratdev01@gmail.com>
2024-11-12 01:21:47 +0500

    end with front starting doing the js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+let myLibrary = [];
 
-  this.info = function () {
-    return `${this.title}, ${this.author}`;
-  };
+
+function Book() {
+  // the constructor
+
+
 }
 
-const book = new Book("HP", "J.K. Rowling", 456, "not read yet");
-console.log(book.info());
 
-const ajjfjkd = 67;
+function addBookToLibrary() {
+  // do stuff here
+
+
+}
